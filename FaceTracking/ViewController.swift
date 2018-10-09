88600b5117244862f793dcfda6cf543aaf186b1e
Will Saults <wsaults@yahoo.com>
2018-10-08 20:07:32 -0500

    Initial commit

---
+++ FaceTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceTracking
+//
+//  Created by Will Saults on 10/8/18.
+//  Copyright © 2018 AppVentures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FaceTrackingTests/FaceTrackingTests.swift
@@ -0,0 +1,34 @@
+//
+//  FaceTrackingTests.swift
+//  FaceTrackingTests
+//
+//  Created by Will Saults on 10/8/18.
+//  Copyright © 2018 AppVentures. All rights reserved.
+//
+
+import XCTest
+@testable import FaceTracking
+
+class FaceTrackingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
